218cad3fbacc166e67f039eebaf75582664b6ae1
HannuKar <69963551+HannuKar@users.noreply.github.com>
2021-03-24 14:44:42 +0200

    Add files via upload

--- html/index.php
+++ html/index.php
@@ -1,5 +1,4 @@
-<?php echo file_get_contents("html/header.html"); ?> 
-<?php echo file_get_contents("html/footer.html"); ?> 
+<?php echo file_get_contents("html/navbar.html"); ?> 
 
 <p> main page. </p>
 
--- html/ohmcalculator.php
+++ html/ohmcalculator.php
@@ -1,28 +1,27 @@
 <?php 
 //include the header and footer for site.
-include("html/footer.html"); 
-include("html/header.html"); 
-include("html/body.html"); 
+include("html/navbar.html"); 
+
 ?>
 
-
-
-
 <html>  
-    <link rel="stylesheet" href="style.css" type="text/css">
-     <link rel="header" href="header.html" type="text/css">
     <head>
-        <title>Ohminlaskuri</title>
+        <title>Ohminlaskuri</title>  
 </head>
 <body>
-    <form action="ohmcalculator.php" method="post">
+
+
+<div>
+
+<form action="ohmcalculator.php" method="post">
+<img src="Images/resist.png" alt="resist" style="float:right;width:242px;height:242px;">
         R1: <input type="text"  name="R1"> <br>
         R2: <input type="text"  name="R2"> <br>
-        R3: <input type="text"  name="R3"> <br>
-        <div>
+        R3: <input type="text"  name="R3"> <br> 
+       
         <input type="submit" value="Send">
-        <div>
 </form >
+
 
 <?php
 $R1 = $_POST["R1"];
@@ -31,5 +30,9 @@ $R3 = $_POST["R3"];
 $totalresistanse = 1/((1/$R1) + (1/$R2) + (1/$R3));
 echo "Total Resistance: " . $totalresistanse;
 ?>
+<div>
+
+
+
 </body>
 </html>
